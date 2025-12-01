1a4f038fcf0626ceb3f1d3ded8391235dda9c430
gourdpastry-prog <gourdpastry@gmail.com>
2025-12-01 15:15:02 -0600

    Update guess the number.cpp
    I forgot to add any prompts.

--- guess the number.cpp
+++ guess the number.cpp
@@ -6,17 +6,20 @@ int main() {
     int input;
     int max;
 
+    std::cout << "Enter the max number.\n";
     std::cin >> max;
     srand(time(0));
     int lucky_number = rand() % max;
     lucky_number + 1;
+    std::cout << "Good number, start guessing.\n\n"
     std::cin >> input;
 
     while (input != lucky_number) {
-        std::cout << "Try again.";
+        std::cout << "Try again.\n";
         std::cin >> input;
     }
 
     std::cout << "Good job, you guessed the number.";
     return 0;
+
 }
